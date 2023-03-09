51c60d229c15f8d4f90ddb200ff50c84e6815313
Mian Hamza <mianhamza@solutionfounder.com>
2023-03-10 01:09:59 +0500

    pre final chages

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App/App";
 import "./index.css";
 import { ConfigProvider } from "antd";
+// import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "#f97e9b",
-          colorLink: "#f97e9b",
-          colorSuccess: "#52c41a",
-          colorWarning: "#faad14",
-          colorError: "#f5222d",
-        },
-      }}
-    >
-      <App />
-    </ConfigProvider>
+    <Router>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#f97e9b",
+            colorLink: "#f97e9b",
+            colorSuccess: "#52c41a",
+            colorWarning: "#faad14",
+            colorError: "#f5222d",
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
+    </Router>
   </React.StrictMode>
 );
